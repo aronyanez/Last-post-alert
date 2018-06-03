6d3811409a04fac45c5219151a1aae1f8af5a836
Unknown <aronyanez@outlook.com>
2018-06-03 15:52:42 -0500

    First File

---
+++ last-posts-alert.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Last Post Alert
+ * Plugin URI: http://markdevs.com
+ * Description: Show an alert with last post.
+ * Version: 1.0.0
+ * Author: Arón Yáñez
+ * Author URI: http://markdevs.com
+ * Requires at least: 4.0
+ * Tested up to: 4.3
+ *
+ * Text Domain: Last Post Alert
+ * Domain Path: /languages/
+ */
+defined( 'ABSPATH' ) or die( '' );
