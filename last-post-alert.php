bbdd5dd0e514aea9a203355cdd12a629953432ba
Unknown <aronyanez@outlook.com>
2018-06-05 16:41:14 -0500

    Change license

--- last-post-alert.php
+++ last-post-alert.php
@@ -6,12 +6,28 @@
  * Version: 1.0.0
  * Author: Arón Yáñez
  * Author URI: http://markdevs.com
- * Requires at least: 4.0
- * Tested up to: 4.3
+ * License:  GPLv2 or later
  *
  * Text Domain: last-post-alert
  * Domain Path: /languages
  */
+
+/*  Copyright 2018 Arón Yáñez (email : aronyanez@markdevs.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
 defined( 'ABSPATH' ) or die( '' );
 
 include_once( 'public/index.php' );
